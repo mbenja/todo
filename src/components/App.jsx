67d3bfd49bfec537284889a5a3191a3e3fe878de
Benjamin Streit <benjaminmstreit@gmail.com>
2020-02-23 21:32:52 -0600

    Some initial configuration.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import TitleBar from "./TitleBar";
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <TitleBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TitleBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/TitleBar.css";
+
+function TitleBar() {
+    return (
+        <div className="titlebar-container">
+            Hello
+        </div>
+    )
+}
+
+export default TitleBar;
